56d45404aabd2a6fc176c525ed403f34c7bbf930
Melissa-Lindberg <melissa.black.noname@gmail.com>
2025-12-03 01:18:08 +0300

    change api

--- my_math.cpp
+++ my_math.cpp
@@ -1,45 +1,50 @@
-#include "math.h"
-
 #include <cerrno>
 #include <climits>
 #include <stdexcept>
 
-static int safe_to_int(const char* s)
+#include "math.h"
+
+bool sum_calculation(int num1, int num2, int &ans)
 {
-    if (s == nullptr) {
-        throw std::runtime_error("String is nullptr");
-    }
-
-    errno = 0;
-    char* end = nullptr;
-    long val = std::strtol(s, &end, 10);
-
-    if (errno == ERANGE) {
-        throw std::runtime_error("Number is out of range");
-    }
-    if (end == s || *end != '\0') {
-        throw std::runtime_error("String isn't number");
-    }
-    if (val < INT_MIN || val > INT_MAX) {
-        throw std::runtime_error("Number is out of range");
-    }
-
-    return static_cast<int>(val);
+    ans = num1 + num2;
+    return true;
 }
 
-static int pow(int num1, int num2)
+bool difference_calculation(int num1, int num2, int &ans)
+{
+    ans = num1 - num2;
+    return true;
+}
+
+bool product_calculation(int num1, int num2, int &ans)
+{
+    ans = num1 * num2;
+    return true;
+}
+
+bool division_calculation(int num1, int num2, int &ans)
+{
+    if (num2 == 0) {
+        return false;
+    }
+    ans = num1 / num2;
+    return true;
+}
+
+bool pow_calculation(int num1, int num2, int &ans)
 {
     if (num2 < 0) {
-        throw std::runtime_error("Power must be >= 0");
+        return false;
     }
     if (num2 == 0) {
         if (num1 == 0) {
-            throw std::runtime_error("0 to the power 0 is undefined");
+            return false;
         } else {
-            return 1;
+            ans = 1;
+            return true;
         }
     }
-    int ans = 1;
+    ans = 1;
     int value = num1;
     int power = num2;
     while (power != 0) {
@@ -49,7 +54,7 @@ static int pow(int num1, int num2)
         power >>= 1;
         value *= value;
     }
-    return ans;
+    return true;
 }
 
 static int factorial_calculation_internal(int num)
@@ -60,57 +65,11 @@ static int factorial_calculation_internal(int num)
     return num * factorial_calculation_internal(num - 1);
 }
 
-int factorial_calculation(char* argv[])
+bool factorial_calculation(int num, int &ans)
 {
-    if (argv[2][0] == '\0' || argv[2][1] != '\0') {
-        throw std::runtime_error("Bad operation");
-    }
-
-    int num = safe_to_int(argv[1]);
     if (num < 0) {
-        throw std::runtime_error("Number is out of range");
+        return false;
     }
-
-    char op = argv[2][0];
-    if (op != '!') {
-        throw std::runtime_error("Bad operation");
-    }
-
-    return factorial_calculation_internal(num);
-}
-
-int opeation_calculation(char* argv[])
-{
-    if (argv[2][0] == '\0' || argv[2][1] != '\0') {
-        throw std::runtime_error("Bad operation");
-    }
-
-    int num1 = safe_to_int(argv[1]);
-    char op = argv[2][0];
-    int num2 = safe_to_int(argv[3]);
-    int ans = 0;
-
-    switch (op) {
-        case '+':
-            ans = num1 + num2;
-            break;
-        case '-':
-            ans = num1 - num2;
-            break;
-        case '*':
-            ans = num1 * num2;
-            break;
-        case '/':
-            if (num2 == 0) {
-                throw std::runtime_error("Division by zero is undefined");
-            }
-            ans = num1 / num2;
-            break;
-        case '^':
-            ans = pow(num1, num2);
-            break;
-        default:
-            break;
-    }
-    return ans;
+    ans = factorial_calculation_internal(num);
+    return true;
 }
--- my_math.h
+++ my_math.h
@@ -1,5 +1,9 @@
 #pragma once
 
-int factorial_calculation(char* argv[]);
+bool sum_calculation(int num1, int num2, int &ans);
+bool difference_calculation(int num1, int num2, int &ans);
+bool product_calculation(int num1, int num2, int &ans);
+bool division_calculation(int num1, int num2, int &ans);
 
-int opeation_calculation(char* argv[]);
+bool pow_calculation(int num1, int num2, int &ans);
+bool factorial_calculation(int num, int &ans);
